375394e900a8c67a7fa992942ccad31dc36734a7
Freaking Wildchild <32355099+gowildchild@users.noreply.github.com>
2026-08-25 06:41:11 +0200

    Create engine_www.js

---
+++ front-end/ui/engine_www.js
@@ -0,0 +1,21 @@
+/** 
+ * * VisualMIX Engine WWW v0.1
+ *** Quick-n-Dirty WWW Routines            VisualMIX/front_end/ui/engine_www.js
+ ** (c)2006-2012 Gunther Voet
+**/
+
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (name + "=")) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
